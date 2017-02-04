f82fb81a00c4d142f13aa49cb754b0ce68feb7a8
vyshnav2003 <puthen1977@gmail.com>
2017-02-04 13:13:18 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req,res) {
+    res.sendfilepath.join(__dirname, 'ui','aricle-one.html');
+});
+
+app.get('/article-two', function (req,res) {
+    res.sendfilepath.join(__dirname, 'ui','aricle-two.html');
+});
+
+app.get('/article-three', function (req,res) {
+    res.sendfilepath.join(__dirname, 'ui','aricle-three.html');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
